1db7e526f186aeab9d7e634a2eddc62255f1a71f
Dmitry Argichev <klikerdima@gmail.com>
2022-10-29 13:23:52 +0300

    1 commit DIMA

--- techprol.cpp
+++ techprol.cpp
@@ -4,6 +4,20 @@ using namespace std ;
 
 class complex_navigation_system {
 public:
+    string name;
+    char vector collection[vec1, vec2, vec3];
+    void add_sensor ();
+    void measure_acc ();
+    void measure_gyro ();
+    void measure_position ();
+    void list_sensors ();
+    void list_sensors_info ();
+};
 
-
+class sensor {
+public:
+    string name;
+    double measurement_unit;
+    int measurement_range [min, max]; 
+    
 };
